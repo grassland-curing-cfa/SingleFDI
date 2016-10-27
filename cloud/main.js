95ce14a978d03ce605b63f8d98712bf97c8e1d04
A.C <grassland.curing.cfa@gmail.com>
2016-10-27 17:00:57 +1100

    Updated package.json, index.js and main.js for initial checkin

--- cloud/main.js
+++ cloud/main.js
@@ -30,6 +30,7 @@ var FILES_OLDER_THAN_DAYS = process.env.FILES_OLDER_THAN_DAYS
 //Use Parse.Cloud.define to define as many cloud functions as you want.
 //For example:
 Parse.Cloud.define("hello", function(request, response) {
+	console.log(JSON.stringify(request));
 	var user = request.user;
 	//var token = user.getSessionToken();
 	console.log(JSON.stringify(user));
